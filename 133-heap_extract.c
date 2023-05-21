c3f0868f7f8adbfce7423e3e5bce38630b671351
Augustinemary Agupugo <austinmary456@gmail.com>
2023-05-21 12:29:08 -0700

    Binary Trees Working

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,13 +1,59 @@
 #include "binary_trees.h"
 
-/**
- * heap_extract - extracts the root node of a Max Binary Heap
- * @root: a double pointer to the root node of heap
- *
- * Return: the value stored in the root node
- *         0 on failure
- */
-int heap_extract(heap_t **root)
-{
+#define INIT_NODE {0, NULL, NULL, NULL}
 
+#define CONVERT "0123456789ABCDEF"
+
+#define SETUP_NODE_BLOC { \
+	tmp = *root; \
+	size = binary_tree_size(*root); \
+	binary = &buffer[49]; \
+	*binary = 0; \
+	}
+
+#define FREE_NODE_BLOC { \
+		res = tmp->n; \
+		free(tmp); \
+		*root = NULL; \
+	}
+
+#define SWAP_HEAD_BLOC { \
+		head = *root; \
+		head = swap_head(head, tmp); \
+		res = head->n; \
+		free(head); \
+		*root = tmp; \
+		tmp = perc_down(tmp); \
+		*root = tmp; \
+	}
+
+#define CONVERT_LOOP { \
+		*--binary = CONVERT[size % 2]; \
+		size /= 2; \
+	}
+
+/**
+ * swap - swaps two nodes in binary tree
+ * @a: first node
+ * @b: second node
+ * Return: pointer to root
+ */
+bst_t *swap(bst_t *a, bst_t *b)
+{
+	bst_t a_copy = INIT_NODE;
+
+	a_copy.n = a->n;
+	a_copy.parent = a->parent;
+	a_copy.left = a->left;
+	a_copy.right = a->right;
+	a->parent = b;
+	a->left = b->left;
+	a->right = b->right;
+	if (b->left)
+		b->left->parent = a;
+	if (b->right)
+		b->right->parent = a;
+
+	b->parent = a_copy.parent;
+	if (a_copy.parent)
 }
